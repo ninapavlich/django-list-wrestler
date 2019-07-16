d81d598540dbc599fc03cd95bcaf5fbf70bb85cb
Nina Pavlich <nina@ninalp.com>
2019-07-16 13:03:26 -0700

    fix to realign versions

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
-#this is a test
-setup(name = 'django-list-wrestler',
-      description = 'Make django admin lists easier to sort and collapse.',
-      version = '6.0',
-      url = 'https://github.com/ninapavlich/django-list-wrestler',
-      author = 'Nina Pavlich',
+# this is a test
+setup(name='django-list-wrestler',
+      description='Make django admin lists easier to sort and collapse.',
+      version='6.1',
+      url='https://github.com/ninapavlich/django-list-wrestler',
+      author='Nina Pavlich',
       author_email='nina@ninalp.com',
-      license = 'BSD',
+      license='BSD',
       packages=find_packages(),
-      package_data={'': ['*.py','*.css','*.js']},
+      package_data={'': ['*.py', '*.css', '*.js']},
       include_package_data=True,
-      install_requires = [],
+      install_requires=[],
       classifiers=[
-            'Development Status :: 4 - Beta',
-            'Environment :: Web Environment',
-            'Framework :: Django',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python'
+          'Development Status :: 4 - Beta',
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python'
       ]
-)
+      )

a6d3d7f16c6a8e5737ab88e73e9196ec2cfff4b5
Nina Pavlich <nina@ninalp.com>
2019-07-16 13:42:25 -0700

    fix missing images in package

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 # this is a test
 setup(name='django-list-wrestler',
       description='Make django admin lists easier to sort and collapse.',
-      version='6.2',
+      version='6.3',
       url='https://github.com/ninapavlich/django-list-wrestler',
       author='Nina Pavlich',
       author_email='nina@ninalp.com',
       license='BSD',
       packages=find_packages(),
-      package_data={'': ['*.py', '*.css', '*.js']},
+      package_data={'': ['*.py', '*.css', '*.js', '*.png']},
       include_package_data=True,
       install_requires=[],
       classifiers=[
